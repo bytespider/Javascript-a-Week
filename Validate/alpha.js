3348b3ce4ef87b9361681b75ef33d95c6336f66f
Rob Griffiths <rob@bytespider.eu>
2011-09-21 22:45:43 +0100

    added unicode regex

--- Validate/alpha.js
+++ Validate/alpha.js
@@ -3,6 +3,9 @@ var exports;
 
     function alpha(value)
     {
+        // match all unicode letters
+        var regex = /[\u0041-\u005A\u0061-\u007A\u00AA\u00B5\u00BA\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u01BF\u01C4-\u0374\u0376\u0377\u037A-\u037D\u0386\u0388-\u038A\u038C\u038E-\u03A1\u03A3-\u07FA\u081A\u0824\u0828\u0971\u0E46\u0EC6]+/;
+        return regex.test(value);
     }
 
     global.JsWeekly = global.JsWeekly || {};
