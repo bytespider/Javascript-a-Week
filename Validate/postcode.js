51929de17b93f4e5a6d0f18dac501ec0b9ad2692
Rob Griffiths <rob@bytespider.eu>
2011-09-21 22:46:54 +0100

    added wikipedia link

--- Validate/postcode.js
+++ Validate/postcode.js
@@ -3,6 +3,7 @@ var exports;
 
     function postcode(value)
     {
+        // http://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
         var regex = /^([A-PR-UWYZ]([0-9]([0-9]|[A-HJKSTUW])?|[A-HK-Y][0-9]([0-9]|[ABEHMNPRVWXY])?)[0-9][ABD-HJLNP-UW-Z]{2}|GIR0AA)$/;
         return regex.test(value);
     }
