3056c01939fa1e23fce5b0a90d109f4697953db0
Rob Griffiths <rob@bytespider.eu>
2011-09-22 17:54:20 +0100

    added iso 8601 date regex

--- Validate/date.js
+++ Validate/date.js
@@ -3,6 +3,9 @@ var exports;
 
     function date(value)
     {
+        // ISO 8601 date
+        var regex = /^(\d{4}-?\d{2}-?\d{2}|\d{4}-\d{2})$/;
+        return regex.test(value);
     }
 
     global.JsWeekly = global.JsWeekly || {};
