eb3cac1a929d7a052a95d7a1dee2e1a442d241ac
Rob Griffiths <rob@bytespider.eu>
2011-09-22 17:54:46 +0100

    Used more accurate postcode from wikipedia

--- Validate/postcode.js
+++ Validate/postcode.js
@@ -4,7 +4,7 @@ var exports;
     function postcode(value)
     {
         // http://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
-        var regex = /^([A-PR-UWYZ]([0-9]([0-9]|[A-HJKSTUW])?|[A-HK-Y][0-9]([0-9]|[ABEHMNPRVWXY])?)[0-9][ABD-HJLNP-UW-Z]{2}|GIR0AA)$/;
+        var regex = /^(GIR 0AA)|(((A[BL]|B[ABDHLNRSTX]?|C[ABFHMORTVW]|D[ADEGHLNTY]|E[HNX]?|F[KY]|G[LUY]?|H[ADGPRSUX]|I[GMPV]|JE|K[ATWY]|L[ADELNSU]?|M[EKL]?|N[EGNPRW]?|O[LX]|P[AEHLOR]|R[GHM]|S[AEGKLMNOPRSTY]?|T[ADFNQRSW]|UB|W[ADFNRSV]|YO|ZE)[1-9]?[0-9]|((E|N|NW|SE|SW|W)1|EC[1-4]|WC[12])[A-HJKMNPR-Y]|(SW|W)([2-9]|[1-9][0-9])|EC[1-9][0-9]) [0-9][ABD-HJLNP-UW-Z]{2})$/;
         return regex.test(value);
     }
 
